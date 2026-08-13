4b85f6b612a2514d50678af7cd13c710a523f340
Luis Quesada <quesadamonterol@gmail.com>
2026-08-13 13:47:15 -0600

    Initial commit

---
+++ src/DashboardSwing.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+import javax.swing.border.*;
+import javax.swing.event.DocumentEvent.*;
+import javax.swing.event.DocumentListener.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableRowSorter;
+import java.awt.*;
+import java.awt.event.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+//Investigar como canviarle el lookAndFeel
+
+public class DashboardSwing extends JFrame {
+
+    //Paleta de colores
+private final Color COLOR_MENU = new Color(255, 255, 255);
+
+
+// Cuadros de texto
+    private JTextField txtNombre;
+
+
+
+}
